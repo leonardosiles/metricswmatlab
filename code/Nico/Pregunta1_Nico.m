e1a8ee11e2e49b479a382ffa2c6c0e91ae51b41b
Nicolás Bastías <nbastias@fen.uchile.cl>
2024-06-10 14:48:46 -0400

    pregunta1

--- code/Nico/Pregunta1_Nico.m
+++ code/Nico/Pregunta1_Nico.m
@@ -1,2 +1,16 @@
 %% Pregunta 1: Tarea 3 (Econometría I).
 
+datos = xlsread("cps09mar.xlsx");
+
+    age = datos(:,1);
+    female = datos(:,2);
+    hisp = datos(:,3);
+    education = datos(:,4);
+    earnings = datos(:,5);
+    hours = datos(:,6);
+    week = datos(:,7);
+    union = datos(:,8);
+    uncov = datos(:,9);
+    region = datos(:,10);
+    race = datos(:,11);
+    marital = datos(:,12);
