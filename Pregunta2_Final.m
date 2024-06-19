d136705e3f6803881a482a7b431b356ae7f795dd
SebaM123 <jmejiasp@fen.uchile.cl>
2024-06-19 12:07:57 -0400

    Update Pregunta2_Final.m

--- Pregunta2_Final.m
+++ Pregunta2_Final.m
@@ -101,101 +101,75 @@ filename = ['densidad_alpha_p1.eps'];
     print(gcf, filename, '-depsc', '-r300');
 
 %% Pregunta 3: Estimación por MC2E.
-clear all   % Se hace clear all. De lo contrario los resultados se traslapan.
+clear all;  % Limpiar todo
+rng(123);   % Semilla
 
-rng(1)               % Semilla.
-
-for a1 = [0.1, 0.5, 1, 5, 10]
-
-N = 1000;
-
+% Parámetros dados
 b0 = 1;
 b1 = 2;
 b2 = 5;
-
 a0 = -4;
-a2 = 3; 
+a2 = 3;
+N = 1000;
+a1_values = [0.1, 0.5, 1, 5, 10];
 
-e_i = randn(N,1);
-u_i = randn(N,1);
-W_i = normrnd(2, 1, N, 1);
-v_i = rand(N, 1); 
-
-Z_i = zeros(N,1); 
-for i = 1:N
-    if v_i(i) < 0.8                       
-       Z_i(i) = 1;  
-    else             
-       Z_i(i) = 0;
-    end
-end
-
-X_i = a0 + a1*Z_i + a2*W_i + u_i; 
-Y = b0 + b1*X_i + b2*W_i + e_i;
-
-% Primera etapa.
-
-Z = [ones(N,1), Z_i];          % Omitimos W_i.
-zeta_gorro = inv(Z'*Z)*Z'*X_i;  
-
-% Segunda etapa.
-
-X_hat = Z*zeta_gorro;
-
-X = [ones(N,1), X_hat];         % Omitimos W_i.
-beta_gorro = inv(X'*X)*X'*Y;
-
-% Errores estándares - Segunda Etapa.
-
-Y = b0 + b1*X_i + e_i;
-X = [ones(N,1), X_hat];         
-beta_gorro_se = inv(X'*X)*X'*Y;
-e_gorro_se = Y - X*beta_gorro_se;
-
-K = length(beta_gorro_se);
-
-s = (e_gorro_se'*e_gorro_se)/(N-K);    % Varianza estimada del error.
-se = sqrt(s*diag(inv(X'*X)));    % Error estándar homocedástico.
-se_r = sqrt(diag(inv(X'*X)*(X'*(diag(e_gorro_se.^2))*(N/(N-K))*X)*inv(X'*X))); % Error estándar robusto.
-
-% Errores estándar - Primera Etapa.
-
-u_gorro = X_i - Z*zeta_gorro;
-
-K = length(zeta_gorro);
-
-s1 = (u_gorro'*u_gorro)/(N-K);    % Varianza estimada del error.
-se1 = sqrt(s1*diag(inv(Z'*Z)));    % Error estándar homocedástico.
-
-se1_r = sqrt(diag(inv(Z'*Z)*(Z'*diag(u_gorro.^2)*(N/(N-K))*Z)*inv(Z'*Z))); % Error estándar robusto.
-
-% Test F (Utiliza errores estándares robustos). 
-
-R = [0 1]';
+for a1 = a1_values
+    % Generación de datos
+    e_i = randn(N, 1);
+    u_i = randn(N, 1);
+    W_i = normrnd(2, 1, N, 1);
+    v_i = rand(N, 1);
+    
+    Z_i = double(v_i < 0.8);
+    
+    X_i = a0 + a1 * Z_i + a2 * W_i + u_i;
+    Y = b0 + b1 * X_i + b2 * W_i + e_i;
+    
+    % Primera etapa
+    Z = [ones(N, 1), Z_i];
+    zeta_gorro = (Z' * Z) \ (Z' * X_i);
+    u_gorro = X_i - Z * zeta_gorro;
+    
+    % Segunda etapa
+    X_hat = Z * zeta_gorro;
+    X = [ones(N, 1), X_hat];
+    beta_gorro = (X' * X) \ (X' * Y);
+    e_gorro = Y - X * beta_gorro;
+    
+    % Errores estándares - Segunda etapa
+    K = length(beta_gorro);
+    s = (e_gorro' * e_gorro) / (N - K);
+    se = sqrt(s * diag(inv(X' * X)));
+    se_r = sqrt(diag(inv(X' * X) * (X' * diag(e_gorro.^2) * (N / (N - K)) * X) * inv(X' * X)));
+    
+    % Errores estándares - Primera etapa
+    K1 = length(zeta_gorro);
+    s1 = (u_gorro' * u_gorro) / (N - K1);
+    se1_r = sqrt(diag(inv(Z' * Z) * (Z' * diag(u_gorro.^2) * (N / (N - K1)) * Z) * inv(Z' * Z)));
+    
+    % Test F
+    R = [0 1]';
     c = 0;
     q = 1;
     var_beta = se1_r(2)^2;
     ftest = (R' * zeta_gorro - c)^2 / var_beta;
-    p_value1 = 1 - fcdf(ftest, q, N - K);
-
-% Resultados.
-
-Primera_Etapa = table(zeta_gorro, se1_r, ...
-    'VariableNames', {'Coeficiente', 'SE Robusto'});
-
-Segunda_Etapa = table(beta_gorro, se, se_r, ...
-    'VariableNames', {'Coeficiente', 'SE', 'SE Robusto'});
-
-disp(['Para un alpha_1 = ', num2str(a1), ', la primera etapa tiene coeficientes y errores estándares:']);
-disp(Primera_Etapa);
-
-disp(['donde el Test F = ', num2str(ftest)]);
-
-disp(['y su segunda etapa será:']);
-disp(Segunda_Etapa);
-
+    p_value1 = 1 - fcdf(ftest, q, N - K1);
+    
+    % Resultados
+    Primera_Etapa = table(zeta_gorro, se1_r, ...
+        'VariableNames', {'Coeficiente', 'SE Robusto'});
+    
+    Pregunta3 = table(beta_gorro, se, se_r, ...
+        'VariableNames', {'Coeficiente', 'SE', 'SE Robusto'});
+    
+    disp(['Para un alpha_1 = ', num2str(a1), ', la primera etapa tiene coeficientes y errores estándar:']);
+    disp(Primera_Etapa);
+    
+    disp(['donde el Test F = ', num2str(ftest)]);
+    
+    disp(['y su segunda etapa será:']);
+    disp(Pregunta3);
 end
-
 %% 4
 % Parametros dados en el problema
 beta0 = 1;
@@ -263,4 +237,3 @@ disp('Sesgo promedio de beta1_hat_MC2E para diferentes valores de alpha1:');
 for j = 1:length(a1_values)
     disp(['alpha1 = ', num2str(a1_values(j)), ', Bias(beta1_hat_MC2E) = ', num2str(bias_beta1_MC2E(j))]);
 end
-
